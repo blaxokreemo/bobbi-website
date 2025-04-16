bd1688fbaeeb82f181b3881351e4332dec2bb444
wyndham <126304806+blaxokreemo@users.noreply.github.com>
2025-04-16 11:33:24 -0400

    Beginning full rework

--- wp-content/themes/bobbi-scratch/page-book.php
+++ wp-content/themes/bobbi-scratch/page-book.php
@@ -11,41 +11,41 @@
             
             if ($theParent or $test_array) { ?>             
 
-      <div class="page-links">
-        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
-        <ul class="min-list">
-          <?php 
-          
-            if ($theParent) {
-                $findChildrenOf = $theParent;
-            } else {
-                $findChildrenOf = get_the_ID();
-            }
+  <div class="page-links">
+    <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
+    <ul class="min-list">
+      <?php 
+      
+        if ($theParent) {
+            $findChildrenOf = $theParent;
+        } else {
+            $findChildrenOf = get_the_ID();
+        }
 
-          wp_list_pages(array(
-            'title_li' => NULL,
-            'child_of' => $findChildrenOf,
-          ));
+      wp_list_pages(array(
+        'title_li' => NULL,
+        'child_of' => $findChildrenOf,
+      ));
 
-          ?>
-        </ul>
-      </div>
+      ?>
+    </ul>
+  </div>
 <?php } ?>
 
-    <?php while(have_posts()) {
-        the_post(); ?>
-        <div class="book-description">
-            <h2><?php the_title(); ?></h2>
-            <div class="book-description-container">
-                <div class="flower-icon"><img src="<?php echo get_theme_file_uri('images/flower.png'); ?>" alt=""></div>
-                <div id="book-description-content">
-                  <?php the_content(); ?>
-                </div>
-                <div class="flower-icon"><img src="<?php echo get_theme_file_uri('images/flower.png'); ?>" alt=""></div>
-            </div>
-        </div>
+<?php while(have_posts()) {
+  the_post(); ?>
+  <div class="book-description">
+      <h2><?php the_title(); ?></h2>
+      <div class="book-description-container">
+          <div class="flower-icon"><img src="<?php echo get_theme_file_uri('images/flower.png'); ?>" alt=""></div>
+          <div id="book-description-content">
+            <?php the_content(); ?>
+          </div>
+          <div class="flower-icon"><img src="<?php echo get_theme_file_uri('images/flower.png'); ?>" alt=""></div>
+      </div>
+  </div>
 
-        <?php } ?>   
+  <?php } ?>   
         
 </div>
 
