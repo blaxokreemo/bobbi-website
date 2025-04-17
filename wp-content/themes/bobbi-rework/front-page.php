2383f66825bfdbc4fcae24e4d7c28cba624b71bc
wyndham <126304806+blaxokreemo@users.noreply.github.com>
2025-04-16 21:10:17 -0400

    Revised front-page.php with re-organized CSS and cleaned up header.css

--- wp-content/themes/bobbi-rework/front-page.php
+++ wp-content/themes/bobbi-rework/front-page.php
@@ -1,25 +1,23 @@
 <?php get_header(); ?>
 
 
-<div class="content-narrow container-flex">
+<div class="container-flex content-narrow">
 
     <?php while(have_posts()) {
         the_post(); ?>
-        <div class="welcome">
-            <h2><?php the_title(); ?></h2>
-            <div class="welcome-container">
+        <div class="box post-wrapper">
+            <h2 class="title-box"><?php the_title(); ?></h2>
+            <div class="post-box">
                 <div class="flower-icon"><img src="<?php echo get_theme_file_uri('images/flower.png'); ?>" alt=""></div>
-                <div id="welcome-content"><?php the_content(); ?></div>
+                <div class="post-content"><?php the_content(); ?></div>
                 <div class="flower-icon"><img src="<?php echo get_theme_file_uri('images/flower.png'); ?>" alt=""></div>
             </div>
         </div>
 
         <?php } ?>
 
-    <div class="announcements">
-        <div class="announcements-title">
-            <h2>Announcements</h2>
-        </div>
+    <div class="box announcements">
+        <h2 class="title-box">Announcements</h2>
         <?php $homepageAnnouncements = new WP_Query(
             array(
               'posts_per_page' => -1,
@@ -30,13 +28,10 @@
         while($homepageAnnouncements->have_posts()) {
             $homepageAnnouncements->the_post(); ?>
             <div class="announcement">
-
-                <div class="announcement-text">
-                    <span class="title-and-date"><h3 class="announcement-title"><a href="<?php the_permalink(); ?>" class="announcement-link"><?php the_title(); ?></a></h3><span class="dipper post-date"><?php the_time('n/d/y'); ?></span></span>
-                    <div class="announcement-info">
-                        <p><?php echo wp_trim_words(get_the_content(), 18); ?></p>
-                        <p class="readmore"><a href="<?php the_permalink(); ?>">Read More</a></p>
-                    </div>
+                <span class="title-and-date"><h3 class="announcement-title"><a href="<?php the_permalink(); ?>" class="announcement-link"><?php the_title(); ?></a></h3><span class="post-date"><?php the_time('n/d/y'); ?></span></span>
+                <div class="announcement-preview">
+                    <p><?php echo wp_trim_words(get_the_content(), 18); ?></p>
+                    <p class="readmore"><a href="<?php the_permalink(); ?>">Read More</a></p>
                 </div>
             </div>
 
---
+++ wp-content/themes/bobbi-rework/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<header class="site-header">
+    <?php if(is_page('25')) { ?>
+    <img class="top-left-image flower-replace" src="<?php echo get_theme_file_uri('images/flower.png'); ?>" alt="">
+    <?php } else { ?>
+    <img class="top-left-image headshot" src="<?php echo get_theme_file_uri('images/headshot.jpg'); ?>" alt="">
+    <?php } ?>
+    <div class="container header-row">
+        <span class="header-line"></span>      
+        <div class="site-title">
+            <?php if(is_page('25')) { ?>
+            <img class="headshot-small-screen" src="<? echo get_theme_file_uri('images/flower.png'); ?>" alt=""> 
+            <?php } else { ?>
+            <img class="headshot-small-screen" src="<?php echo get_theme_file_uri('images/headshot.jpg'); ?>" alt="">
+            <?php } ?>
+            <h1 class="site-title-text"><a href="<?php echo site_url(); ?>">ROBERTA SCHNORR</a></h1>
+        </div>
+        <nav class="site-nav">
+            <ul>
+                <li><a href="<?php echo site_url('/about') ?>">About</a></li>
+                <li><a href="<?php echo site_url('/book') ?>">Book</a></li>
+                <li><a href="https://rschnorr.substack.com/">Substack</a></li>
+            </ul>
+        </nav>
+        </div>
+    </div>
+
+</header>
+<body <?php body_class(); ?>>
