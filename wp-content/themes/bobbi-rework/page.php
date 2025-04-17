8d7a5016388dea35620dac372ab986f2e34261e1
wyndham <126304806+blaxokreemo@users.noreply.github.com>
2025-04-17 13:29:42 -0400

    Turned the side menu into a template part. Added dynamic styling for current_page_item

--- wp-content/themes/bobbi-rework/page.php
+++ wp-content/themes/bobbi-rework/page.php
@@ -4,35 +4,8 @@
 <div class="container-flex room-for-menu content-wide">
 
 <?php
-          $theParent = wp_get_post_parent_id(get_the_ID());
-          $test_array = get_pages(array(
-              'child_of' => get_the_ID()
-          ));
-          
-          if ($theParent or $test_array) { ?>             
-
-    <div class="side-menu-background box">
-      <div class="page-links">
-        <h2 class="title-box"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
-        <ul class="min-list">
-          <?php 
-          
-            if ($theParent) {
-                $findChildrenOf = $theParent;
-            } else {
-                $findChildrenOf = get_the_ID();
-            }
-
-          wp_list_pages(array(
-            'title_li' => NULL,
-            'child_of' => $findChildrenOf,
-          ));
-
-          ?>
-        </ul>
-      </div>
-    </div>
-<?php } ?>
+  get_template_part('template-parts/side-menu');
+?>
 
   <?php if (the_post_thumbnail()) { ?>
       <div class="featured-image-container">
--- wp-content/themes/bobbi-rework/template-parts/side-menu.php
+++ wp-content/themes/bobbi-rework/template-parts/side-menu.php
@@ -1,11 +1,11 @@
 <?php
-          $theParent = wp_get_post_parent_id(get_the_ID());
-          $test_array = get_pages(array(
-              'child_of' => get_the_ID()
-          ));
-          
-          if ($theParent or $test_array) { ?>             
-
+  $theParent = wp_get_post_parent_id(get_the_ID());
+  $test_array = get_pages(array(
+      'child_of' => get_the_ID()
+  ));
+  
+  if ($theParent or $test_array) { ?>
+    
     <div class="side-menu-background box">
       <div class="page-links">
         <h2 class="title-box"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
@@ -28,4 +28,4 @@
       </div>
     </div>
 
-<?php } ?>
+  <?php } ?>
