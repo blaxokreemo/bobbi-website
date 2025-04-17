c4e2ab95a351ee42434f44010ec132f8189c96dd
wyndham <126304806+blaxokreemo@users.noreply.github.com>
2025-04-16 21:38:16 -0400

    Copied remaining files into bobbi-rework, all CSS is reorganized

---
+++ wp-content/themes/bobbi-rework/page-about.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+
+
+<div class="content-narrow container-flex">
+
+    <div class="headshot-container">
+        <img class="headshot-large" src="<?php echo get_theme_file_uri('images/headshot-large.jpg') ?>" alt="">
+    </div>
+
+    <?php while(have_posts()) {
+        the_post(); ?>
+        <div class="box post-wrapper">
+            <h2 class="title-box">About the Author</h2>
+            <div class="post-box">
+                <div class="flower-icon"><img src="<?php echo get_theme_file_uri('images/flower.png'); ?>" alt=""></div>
+                <div class="post-content"><?php the_content(); ?></div>
+                <div class="flower-icon"><img src="<?php echo get_theme_file_uri('images/flower.png'); ?>" alt=""></div>
+            </div>
+        </div>
+
+        <?php } ?>   
+        
+</div>
+
+</div>
+
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/bobbi-rework/page-book.php
@@ -0,0 +1,55 @@
+<?php get_header(); ?>
+
+
+<div class="content-wide container-flex page-book">
+
+<?php
+            $theParent = wp_get_post_parent_id(get_the_ID());
+            $test_array = get_pages(array(
+                'child_of' => get_the_ID()
+            ));
+            
+            if ($theParent or $test_array) { ?>             
+
+  <div class="page-links">
+    <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
+    <ul class="min-list">
+      <?php 
+      
+        if ($theParent) {
+            $findChildrenOf = $theParent;
+        } else {
+            $findChildrenOf = get_the_ID();
+        }
+
+      wp_list_pages(array(
+        'title_li' => NULL,
+        'child_of' => $findChildrenOf,
+      ));
+
+      ?>
+    </ul>
+  </div>
+<?php } ?>
+
+<?php while(have_posts()) {
+  the_post(); ?>
+  <div class="box post-wrapper">
+      <h2 class="title-box"><?php the_title(); ?></h2>
+      <div class="post-box">
+          <div class="flower-icon"><img src="<?php echo get_theme_file_uri('images/flower.png'); ?>" alt=""></div>
+          <div class="post-content">>
+            <?php the_content(); ?>
+          </div>
+          <div class="flower-icon"><img src="<?php echo get_theme_file_uri('images/flower.png'); ?>" alt=""></div>
+      </div>
+  </div>
+
+  <?php } ?>   
+        
+</div>
+
+</div>
+
+
+<?php get_footer(); ?>
